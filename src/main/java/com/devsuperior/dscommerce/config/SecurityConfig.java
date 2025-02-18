b20d22d3fe1d15f4655bd3c7265e0fcf39806fef
wagnercaf <wagnercaf@hotmail.com>
2025-02-18 17:09:47 -0300

    Spring Security

--- src/main/java/com/devsuperior/dscommerce/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/dscommerce/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscommerce.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,9 +11,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        http.securityMatcher(PathRequest.toH2Console())
+                .csrf(csrf -> csrf.disable())
+                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+
         return http.build();
     }
-
 }
