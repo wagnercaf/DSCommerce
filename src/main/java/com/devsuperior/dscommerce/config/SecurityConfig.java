4fee71f578fec72002099bafaece56c3194645cf
wagnercaf <wagnercaf@hotmail.com>
2025-02-25 14:59:50 -0300

    BCrypt password

--- src/main/java/com/devsuperior/dscommerce/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/dscommerce/config/SecurityConfig.java
@@ -4,10 +4,17 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+     return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
